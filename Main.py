b4c0ac251433b9640ebe4fc370429228db3a2513
AlexDev <130192838+AlexDev055@users.noreply.github.com>
2024-11-11 20:24:19 -0600

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,17 +1,14 @@
 from scapy.all import sniff
 
-# Definir una función de análisis de paquetes
 def analizar_paquete(paquete):
     if paquete.haslayer("IP"):
         ip_origen = paquete["IP"].src
         ip_destino = paquete["IP"].dst
         print(f"Paquete detectado de {ip_origen} a {ip_destino}")
         
-        # Verificar intentos sospechosos
+    
         if paquete.haslayer("TCP"):
             puerto_destino = paquete["TCP"].dport
             print(f"Intento de conexión a puerto: {puerto_destino}")
-            # Aquí puedes añadir lógica para alertas si detectas muchos intentos desde la misma IP
-            
-# Capturar tráfico en la red
+   
 sniff(prn=analizar_paquete, store=0, count=100)
